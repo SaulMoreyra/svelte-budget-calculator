92450cd63fe7d7c80697a7381bed2b8c6bd9e692
SaulMoreyra <armhacam21@gmail.com>
2021-12-29 19:55:11 -0600

    fix(githubUsers): add component to show github users

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,8 @@
 <script>
   import { afterUpdate, onMount, setContext } from "svelte";
 
-  import GithubAwait from './components/GithubAwait.svelte'
-  // import Github from './components/Github.svelte'
+  import GithubAwait from "./components/GithubAwait.svelte";
+  import Github from "./components/Github.svelte";
   import Navbar from "./components/Navbar.svelte";
   import ExpensesList from "./components/ExpensesList.svelte";
   import Totals from "./components/Totals.svelte";
@@ -25,12 +25,12 @@
 
   //FUNCTIONS
 
-  function showForm(){
+  function showForm() {
     isFormOpen = true;
   }
 
-  function closeForm(){
-    isFormOpen = false;    
+  function closeForm() {
+    isFormOpen = false;
     editableExpense = { ...defaultExpense };
   }
 
@@ -44,11 +44,11 @@
 
   function addExpense({ name, amount }) {
     const newExpense = { id: Math.random(), name, amount };
-    expenses = [newExpense, ...expenses];    
+    expenses = [newExpense, ...expenses];
   }
 
   function setModifiedExpense(id) {
-    showForm()
+    showForm();
     editableExpense = expenses.find((expense) => expense.id === id);
   }
 
@@ -59,18 +59,18 @@
     editableExpense = { ...defaultExpense };
   }
 
-  function setLocalStorage(){
+  function setLocalStorage() {
     localStorage.setItem("expenses", JSON.stringify(expenses));
   }
 
-  onMount(()=> {
-    const storageExpenses = localStorage.getItem("expenses")
-    expenses = storageExpenses? JSON.parse(storageExpenses) : [];
-  })
+  onMount(() => {
+    const storageExpenses = localStorage.getItem("expenses");
+    expenses = storageExpenses ? JSON.parse(storageExpenses) : [];
+  });
 
-  afterUpdate(()=> {
-    setLocalStorage()
-  })
+  afterUpdate(() => {
+    setLocalStorage();
+  });
 
   //CONTEXT
   setContext("expenses", {
@@ -86,25 +86,23 @@
 </script>
 
 <!-- HTML -->
-<Navbar {showForm}/>
+<Navbar {showForm} />
 <main class="content">
-  <!-- 
   {#if isFormOpen}
-  <Modal>
-    <ExpenseForm
-    {addExpense}
-    {editExpense}
-    {closeForm}
-    {isEditing}
-    name={editableExpense.name}
-    amount={editableExpense.amount}
-    />
-  </Modal>
+    <Modal>
+      <ExpenseForm
+        {addExpense}
+        {editExpense}
+        {closeForm}
+        {isEditing}
+        name={editableExpense.name}
+        amount={editableExpense.amount}
+      />
+    </Modal>
   {/if}
   <Totals title="total expenses" {total} /> -->
   <!-- THIS IS IF YOU WANT TO USE A EVENTDISPATCHER INSTED OF CONTEXT -->
-  <!-- <ExpensesList {expenses} on:delete={deleteExpenseDispatch} /> -->
-  <!-- <ExpensesList {expenses} />
+  <ExpensesList {expenses} />
   <button
     type="button"
     class="btn btn-primary btn-block"
@@ -112,6 +110,7 @@
   >
     clear expenses
   </button>
--->
-<GithubAwait/>
+
+  <GithubAwait />
+  <Github />
 </main>
